8cdaa1d32e7add1c802463c84983d2695d273011
Vinicius Borges <viniborges2005@gmail.com>
2024-01-22 20:31:40 -0300

    Atualizacao

--- Program.cs
+++ Program.cs
@@ -9,13 +9,20 @@ namespace Xadrez_Concole
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta),new Posicao(2,4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
         }
