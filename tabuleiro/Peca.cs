a3d018d932a995fb29756201aeade8761427999e
Vinicius Borges <viniborges2005@gmail.com>
2024-02-29 10:42:15 -0300

    Correcao: mudando o nome do metodo para movimentoPossivel

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -47,7 +47,7 @@ namespace tabuleiro
             return false;
         }
 
-        public bool podeMoverPara(Posicao pos)
+        public bool movimentoPossivel(Posicao pos)
         {
             return movimentosPossiveis()[pos.linha, pos.coluna];
         }
